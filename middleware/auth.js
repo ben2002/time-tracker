e0dad2cfac5cbea6ba7c93fc52655354f9b6676a
Ben Moser <benjamin.moser@hs-augsburg.de>
2020-05-16 21:33:49 +0200

    fixed auth error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 var jwt = require('jsonwebtoken');
 require('dotenv').config();
+const config = require('config');
 
 module.exports = function (req, res, next) {
 	// get the token from header
@@ -9,7 +10,7 @@ module.exports = function (req, res, next) {
 	if (!token) return res.status(401).json({ msg: 'no token, authorization denied' });
 
 	// verify the token
-	jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+	jwt.verify(token, config.get('JWT_SECRET'), (err, decoded) => {
 		if (err) {
 			return res.status(401).json({ msg: 'invalid token' });
 		}
